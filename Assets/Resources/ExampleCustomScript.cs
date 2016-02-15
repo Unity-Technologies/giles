0839133b0d6036b216c69920c9413ebdb79f01f3
Eitan Elbaz <eitan_elbaz@hotmail.co.uk>
2016-02-15 18:53:14 +0000

    remove attribute example usage script

--- Assets/Resources/ExampleCustomScript.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-[GILES.Serialization.pb_InspectorName("My custom name")]
-public class ExampleCustomScript : MonoBehaviour {
-
-    public string something = "o.O";
-}
