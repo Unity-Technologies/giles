92548b810646d7d87ec84aaf522d00144e7ec28c
FAL2009 <fabio.lugert0@gmail.com>
2019-09-15 19:09:55 +0200

    Enum Inspector
    The enum inspector is now included.

--- Assets/GILES/Code/Classes/GUI/Type Inspectors/pb_EnumInspector.cs
+++ Assets/GILES/Code/Classes/GUI/Type Inspectors/pb_EnumInspector.cs
@@ -1,74 +1,69 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace GILES.Interface
 {
-	/**
+    /**
 	 * Field editor for enum types.
 	 */
-	[pb_TypeInspector(typeof(System.Enum))]
-	public class pb_EnumInspector : pb_TypeInspector
-	{
-		object value;
+    [pb_TypeInspector(typeof(System.Enum))]
+    public class pb_EnumInspector : pb_TypeInspector
+    {
+        object value;
 
-		public Text title;
-		public Button button;
-		public Text currentEnumValue;
-		string[] enumNames;		/// the available names of this enum
-		System.Array enumValues;	/// the available values of this enum
+        public Text title;
+        public Dropdown dropdown;
+        string[] enumNames;		/// the available names of this enum
+		System.Array enumValues;    /// the available values of this enum
 
-		void OnGUIChanged()
-		{
-			SetValue(value);
-		}
+        void OnGUIChanged()
+        {
+            SetValue(value);
+        }
 
-		public override void InitializeGUI()
-		{
-			title.text = GetName().SplitCamelCase();
-			button.onClick.AddListener( OnClick );
+        public override void InitializeGUI()
+        {
+            title.text = GetName().SplitCamelCase();
+            dropdown.onValueChanged.AddListener(OnClick);
 
-			enumNames = System.Enum.GetNames(declaringType);
-			enumValues = System.Enum.GetValues(declaringType);
-		}
+            enumNames = System.Enum.GetNames(declaringType);
+            enumValues = System.Enum.GetValues(declaringType);
+            RefreshDropdown();
+        }
 
-		void OnClick()
-		{
-			// cycle enum value
-			if(enumValues != null)
-			{
-				int len = enumValues.Length;
+        void OnClick(int index)
+        {
+            // cycle enum value
+            if (enumValues != null)
+            {
+                int len = enumValues.Length;
+                value = enumValues.GetValue(index);
 
-				// values may not be linear, or integers at all
-				int index = System.Array.IndexOf(enumValues, value);
+                OnGUIChanged();
+            }
+        }
 
-				index++;
+        protected override void OnUpdateGUI()
+        {
+            value = GetValue<object>();
 
-				if(index >= len)
-					index = 0;
+            RefreshDropdown();
+        }
 
-				value = enumValues.GetValue(index);
+        void RefreshDropdown()
+        {
+            dropdown.ClearOptions();
+            List<string> options = new List<string>();
 
-				RefreshText();
-				
-				OnGUIChanged();
-			}
-		}
+            for (int i = 0; i < enumValues.Length; i++)
+            {
+                options.Add(enumNames[i].ToString());
+            }
+            dropdown.AddOptions(options);
+        }
 
-		protected override void OnUpdateGUI()
-		{
-			value = GetValue<object>();
 
-			RefreshText();
-		}
-
-		void RefreshText()
-		{
-			if(enumNames != null)
-				currentEnumValue.text = value.ToString();// enumNames[value];
-			else
-				currentEnumValue.text = "Enum: " + value.ToString();
-		}
-	}
+    }
 }
