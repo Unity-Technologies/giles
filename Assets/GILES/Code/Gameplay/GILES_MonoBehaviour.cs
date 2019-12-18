a982905f749c1531ba2330849485cc83f7cdc31b
FAL2009 <46822942+FAL2009@users.noreply.github.com>
2019-12-18 13:49:04 +0100

    Create GILES_MonoBehaviour.cs

---
+++ Assets/GILES/Code/Gameplay/GILES_MonoBehaviour.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace GILES.Gameplay
+{
+    public class GILES_MonoBehaviour  : MonoBehaviour {
+      public virtual void OnUpdate(){
+      
+      }
+      public virtual void OnStart(){
+      
+      }
+      public virtual void OnAwake(){
+      
+      }
+      public virtual void OnLateUpdate(){
+      
+      }
+    }
+}
