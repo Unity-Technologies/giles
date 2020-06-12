5a333cc8f6f6998824a8cfc87d44af8aee4e0565
FAL2009 <46822942+FAL2009@users.noreply.github.com>
2020-06-12 13:08:58 +0200

    Update pb_InputExtension.cs
    Added xml tags

--- Assets/GILES/Code/Classes/Extension/pb_InputExtension.cs
+++ Assets/GILES/Code/Classes/Extension/pb_InputExtension.cs
@@ -9,12 +9,16 @@ namespace GILES
 	 */
 	public static class pb_InputExtension
 	{
-
+		///<summary>
+		/// Returns true if any shift button is pressed.
+		///</summary>
 		public static bool Shift()
 		{
 			return Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
 		}
-
+		///<summary>
+		/// Returns true if any control button is pressed.
+		///</summary>
 		public static bool Control()
 		{
 			return Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
